f1b50c6357059b843fa484151815b3e5f1606ab0
Nsuliman <nsuliman88@gmail.com>
2020-01-29 05:27:10 +0200

    starting testing

--- __tests__/categories.test.js
+++ __tests__/categories.test.js
@@ -1,66 +1,28 @@
 'use strict';
 
-require('@code-fellows/supergoose');
+const supergoose = require('@code-fellows/supergoose');
 
-const Categories = require('../models/categories-mod/categories-model.js');
+const category = require(`../models/categories-mod/categories-model.js`);
 
-const category = new Categories();
+let testObject = {name :'bags bags'};
 
-describe(' Categories Testing ', () => {
+describe('categories Model', () => {
+  it('can create() a new categories', () => {
+    return category.create(testObject)
+      .then(record => {
+        Object.keys(testObject).forEach(key => {
+          expect(record[key]).toEqual(testObject[key]);
+        });
+      });
+  });
 
-    it(' can get() any category ', () => {
-        let newRec = { name: 'test category' };
+  it('can get() categories', () => {
+    return category.get()
+      .then(foundObject => {
+        Object.keys(testObject).forEach(key => {
+          expect(foundObject[0][key]).toEqual(testObject[key]);
+        });
+      });
+  });
 
-        return category.create(newRec)
-            .then(record => {
-                return category.get(record._id)
-                    .then(catItem => {
-                        Object.keys(newRec).forEach(key => {
-                            expect(catItem[key]).toEqual(newRec[key]);
-                        }) // end of forEach 
-                    }) // end of then promise (catItem)
-            }) // end of then promise (record)
-            .catch(e => console.error('ERR', e));
-    }) // end of it  get ()
-
-
-    it(' can create() A new category ', () => {
-        let newRec = { name: 'test category' };
-        return category.create(newRec)
-            .then(record => {
-                Object.keys(newRec).forEach(key => {
-                    expect(record[key]).toEqual(newRec[key]);
-                }) // end of forEach 
-            }) // end of then promise (record)
-            .catch(e => console.error('ERR', e));
-    }); // end of it  create ()
-
-    it(' can update() A category ', () => {
-        let newRec = { name: 'test category' };
-        return category.create(newRec)
-            .then(record => {
-                return category.update(record._id, record)
-                    .then(catItem => {
-                        Object.keys(newRec).forEach(key => {
-                            expect(catItem[key]).toEqual(newRec[key]);
-                        }) // end of forEach 
-                    }) // nd of then promise (catItem)
-            }) // end of then promise (record)
-            .catch(e => console.error('ERR', e));
-    }); // end of it update ()
-
-    it(' can Delete() A category ', () => {
-        let newRec = { name: 'test category' };
-        return category.create(newRec)
-            .then(record => {
-                return category.delete(record._id, record)
-                    .then(catItem => {
-                        Object.keys(newRec).forEach(key => {
-                            expect(catItem[key]).toEqual(newRec[key]);
-                        }) // end of forEach 
-                    }) // nd of then promise (catItem)
-            }) // end of then promise (record)
-            .catch(e => console.error('ERR', e));
-    }); // end of it Delete ()
-
-}) // end of Categories 
+});s
